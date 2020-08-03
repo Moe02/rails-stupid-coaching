8c836774ef794255e9ec5bc5899c9dced4aaf3e1
Moe Aoki <mt215tjk@gmail.com>
2020-08-03 12:51:56 +0900

    No test yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'questions#home' # when no path, go here
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
+  # get '/contact', to: 'pages#contact'
 end
